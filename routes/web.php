4f33e16fbc23a494728aa7d60d153dfc7b087246
mdrdani <sudokill3004@gmail.com>
2022-09-22 10:13:29 +0700

    Install Laravel Jetstream & Modify Dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', BookingOnline::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
